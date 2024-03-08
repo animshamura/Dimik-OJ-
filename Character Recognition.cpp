1261a5a027284cd89c19b9a4592961051094ae24
Shamura Ahmad Anim <47661479+animshamura@users.noreply.github.com>
2024-03-09 00:19:28 +0600

    Create Character Recognition.cpp

---
+++ Character Recognition.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    for (int i = 0; i < t; i++){
+        char a;
+        cin>>a;
+        if(a>='A' &&  a<='Z') cout<<"Uppercase Character"<<endl;
+        else if (a>='a' && a<='z') cout<< "Lowercase Character"<<endl;
+        else if(a>='0' && a<='9') cout<< "Numerical Digit"<<endl;
+        else cout<<"Special Characters"<<endl;
+    }
+    return 0;
+}
